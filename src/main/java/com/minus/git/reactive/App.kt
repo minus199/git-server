3269ce5c4f4776dc479c14a0a5b09a51dea3ac19
Asaf Blum <minus199@gmail.com>
2021-09-17 23:25:22 +0300

    pom - io.projectreactor.reactor-tools and reactor-kotlin-extensions app props

--- src/main/java/com/minus/git/reactive/App.kt
+++ src/main/java/com/minus/git/reactive/App.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.PropertySource
 import org.springframework.context.annotation.PropertySources
+import reactor.tools.agent.ReactorDebugAgent
 
 
 @PropertySources(
